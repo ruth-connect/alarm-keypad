424676d9211f7092374ef4f7e80d6a0023cefae0
ruth <ruth@DESKTOP-HOVL7PT>
2020-08-20 16:03:43 +0100

    Initial creation from Spring Initializr.

---
+++ src/main/java/uk/me/ruthmills/alarmkeypad/AlarmkeypadApplication.java
@@ -0,0 +1,13 @@
+package uk.me.ruthmills.alarmkeypad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlarmkeypadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlarmkeypadApplication.class, args);
+	}
+
+}
---
+++ src/test/java/uk/me/ruthmills/alarmkeypad/AlarmkeypadApplicationTests.java
@@ -0,0 +1,13 @@
+package uk.me.ruthmills.alarmkeypad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlarmkeypadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

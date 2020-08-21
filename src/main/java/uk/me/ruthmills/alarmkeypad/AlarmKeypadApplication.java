d56431cba05a177404b3fd146909f6a2314550c0
ruth <ruth@DESKTOP-HOVL7PT>
2020-08-21 16:32:17 +0100

    Enable scheduling.

--- src/main/java/uk/me/ruthmills/alarmkeypad/AlarmKeypadApplication.java
+++ src/main/java/uk/me/ruthmills/alarmkeypad/AlarmKeypadApplication.java
@@ -2,8 +2,10 @@ package uk.me.ruthmills.alarmkeypad;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class AlarmKeypadApplication {
 
 	public static void main(String[] args) {

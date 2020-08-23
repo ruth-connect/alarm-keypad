407f0f041cfdecf0cfdf17ef91cdf1a7b211fd15
ruth <ruth@DESKTOP-HOVL7PT>
2020-08-23 19:57:48 +0100

    Add exception logging.

--- src/main/java/uk/me/ruthmills/alarmkeypad/polling/AlarmStatePoller.java
+++ src/main/java/uk/me/ruthmills/alarmkeypad/polling/AlarmStatePoller.java
@@ -1,5 +1,7 @@
 package uk.me.ruthmills.alarmkeypad.polling;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
@@ -12,8 +14,14 @@ public class AlarmStatePoller {
 	@Autowired
 	private AlarmStateService alarmStateService;
 
+	private final Logger logger = LoggerFactory.getLogger(AlarmStatePoller.class);
+
 	@Scheduled(cron = "*/1 * * * * *")
 	public void tick() {
-		alarmStateService.tick();
+		try {
+			alarmStateService.tick();
+		} catch (Exception ex) {
+			logger.error("Exception in poller thread", ex);
+		}
 	}
 }
